b5b7264f3fe7570668d9aa22bad726e2d3c7e327
Shamonti <shamonti.haque98@gmail.com>
2024-05-20 12:43:18 +0600

    Used Flex

--- chakra-ui-for-beginners/src/App.tsx
+++ chakra-ui-for-beginners/src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Spacer } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className='App'>
-      <Box as='section' bg='tomato' w='120px' h='150px' color='white'>
-        This is a box.
-      </Box>
+      <Flex justify='space-between' align='center'>
+        <Box as='section' bg='tomato' w='120px' h='150px' color='white'>
+          This is a box.
+        </Box>
+        <Spacer />
+        <Box as='section' bg='yellow' w='120px' h='100px' color='white'>
+          This is a box.
+        </Box>
+        <Box as='section' bg='red' w='140px' h='100px' color='white'>
+          This is a box.
+        </Box>
+        <Box as='section' bg='teal' w='150px' h='150px' color='white'>
+          This is a box.
+        </Box>
+      </Flex>
     </div>
   );
 }
